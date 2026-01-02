895abf0b2dd0e5c1918f3ff998cb6b2e108298e0
PLParadis <31371823+plparadis@users.noreply.github.com>
2026-01-02 21:09:56 +0000

    fix from pre-commit workflow

--- custom_components/gmp_ha/__init__.py
+++ custom_components/gmp_ha/__init__.py
@@ -9,24 +9,25 @@ from greenmountainpower import exceptions as gmp_exceptions
 from greenmountainpower.api import UsagePrecision
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import ConfigEntryAuthFailed, ConfigEntryNotReady
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+from homeassistant.exceptions import ConfigEntryAuthFailed
+from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from homeassistant.helpers.update_coordinator import UpdateFailed
 from homeassistant.util import dt as dt_util
 
-from .api import GmpClient, HourlyUsage
-from .const import (
-    CONF_ACCOUNT_NUMBER,
-    CONF_BACKFILL_DAYS,
-    CONF_PASSWORD,
-    CONF_PRICE_PER_KWH,
-    CONF_USERNAME,
-    DEFAULT_BACKFILL_DAYS,
-    DEFAULT_PRICE_PER_KWH,
-    DOMAIN,
-    NAME,
-    PLATFORMS,
-    SCAN_INTERVAL,
-)
+from .api import GmpClient
+from .api import HourlyUsage
+from .const import CONF_ACCOUNT_NUMBER
+from .const import CONF_BACKFILL_DAYS
+from .const import CONF_PASSWORD
+from .const import CONF_PRICE_PER_KWH
+from .const import CONF_USERNAME
+from .const import DEFAULT_BACKFILL_DAYS
+from .const import DEFAULT_PRICE_PER_KWH
+from .const import DOMAIN
+from .const import NAME
+from .const import PLATFORMS
+from .const import SCAN_INTERVAL
 
 _LOGGER = logging.getLogger(__name__)
 
--- custom_components/gmp_ha/config_flow.py
+++ custom_components/gmp_ha/config_flow.py
@@ -4,25 +4,24 @@ from __future__ import annotations
 
 import logging
 
+import voluptuous as vol
 from greenmountainpower import exceptions as gmp_exceptions
 from homeassistant import config_entries
 from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import HomeAssistantError
 from oauthlib.oauth2.rfc6749 import errors as oauth_errors
-import voluptuous as vol
+
 
 from .api import GmpClient
-from .const import (
-    CONF_ACCOUNT_NUMBER,
-    CONF_BACKFILL_DAYS,
-    CONF_PASSWORD,
-    CONF_PRICE_PER_KWH,
-    CONF_USERNAME,
-    DEFAULT_BACKFILL_DAYS,
-    DEFAULT_PRICE_PER_KWH,
-    DOMAIN,
-)
+from .const import CONF_ACCOUNT_NUMBER
+from .const import CONF_BACKFILL_DAYS
+from .const import CONF_PASSWORD
+from .const import CONF_PRICE_PER_KWH
+from .const import CONF_USERNAME
+from .const import DEFAULT_BACKFILL_DAYS
+from .const import DEFAULT_PRICE_PER_KWH
+from .const import DOMAIN
 
 
 _LOGGER = logging.getLogger(__name__)
